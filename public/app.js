fdfe17e423b9bfe66c015e1a7ca930d247234358
Andrei Laslo <andreil.ltai@yahoo.com>
2015-07-22 16:09:14 +0300

    updates (5) on main page - main form working for contacts and reservations + controlle updates

--- public/app.js
+++ public/app.js
@@ -15,49 +15,16 @@ bookIt.config(['$routeProvider',
       });
   }]);
 
-bookIt.controller('ZoneListController', function ($scope, $http) {
-	$http.get('/zones').
-	  success(function(data) { 
-	    $scope.zones = data;
-	  })
-  
-});
-
-bookIt.controller('DTablePickerController', function ($scope, $http) {
-  $http.get('/dining_tables').
-    success(function(data) {
-      $scope.dining_tables = data;
-    })
-  
-});
-
 bookIt.controller('DatepickerDemoCtrl', function ($scope) {
  
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
-
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
+  
 
   // // Disable weekend selection
   // $scope.disabled = function(date, mode) {
   //   return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
   // };
 
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
 
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-
-    $scope.opened = true;
-  };
 
   // $scope.dateOptions = {
   //   formatYear: 'yy',
@@ -83,21 +50,7 @@ bookIt.controller('DatepickerDemoCtrl', function ($scope) {
   //     }
   //   ];
 
-  $scope.getDayClass = function(date, mode) {
-    if (mode === 'day') {
-      var dayToCheck = new Date(date).setHours(0,0,0,0);
 
-      for (var i=0;i<$scope.events.length;i++){
-        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
-        }
-      }
-    }
-
-    return '';
-  };
 });
 
 bookIt.controller('FormController', ['$scope', '$http', function($scope, $http) {
@@ -106,61 +59,75 @@ bookIt.controller('FormController', ['$scope', '$http', function($scope, $http) 
       $scope.contact = {
         first_name: 'asd'
       };
+      $scope.dining_tables = [];
+
+      $http.get('/zones').
+        success(function(data) { 
+        $scope.zones = data;
+      })
+
+      $http.get('/dining_tables').
+        success(function(data) {
+        $scope.dining_tables = data;
+      })
+
+      $scope.today = function() {
+        $scope.dt = new Date();
+      };
+        $scope.today();
+
+      $scope.clear = function () {
+        $scope.dt = null;
+      };
+
+      $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+      };
+      $scope.toggleMin();
+
+      $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+      };
+
+      $scope.getDayClass = function(date, mode) {
+        if (mode === 'day') {
+          var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+          for (var i=0;i<$scope.events.length;i++){
+            var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+            if (dayToCheck === currentDay) {
+              return $scope.events[i].status;
+            }
+          }
+        }
+
+        return '';
+      };
 
       $scope.update = function(contact) {
         $scope.master = angular.copy(contact);
       };
 
-      $scope.foo = function(){
-        
-        alert('foo');
-        
-        // $http({
-        //     method: 'POST',
-        //     url: '/contacts',
-        //     data: JSON.stringify($scope.contact),
-        //     }).
-        // success(function (data) {
-        //     //Showing Success message
-        //     $scope.status = "The Person Saved Successfully!!!";
-        //     //Loading people to the $scope
-        //     GetContacts();
-        // })
-        // .error(function (error) {
-        //     //Showing error message
-        //     $scope.status = 'Unable to create a person: ' + error.message;
-        // }); 
-      }
-      
-      // function GetContacts() {
-      //   //Defining the $http service for getting the people
-      //   $http.get('/contacts')
-      //   success(function (data) {
-      //       if (data != null || data != 'undefined') {
-      //          //Assigning people data to the $scope variable
-      //           $scope.contacts = data;
-      //           //Clearing the Person object in create context and Showing default Gender(Male) Checked
-      //           $scope.contact = {
-                   
-      //              first_name: '',
-      //              last_name: '',
-      //              phone_number:'',
-      //              email:'', 
-      //          };
-      //      }
-      //   })
-      //   .error(function (error) {
-      //       //Showing error message
-      //       $scope.status = 'Unable to retrieve contacts' + error.message;
-      //   });
-      // }
-
       $scope.createContact = function () {
-
+        params = {
+          first_name: $scope.contact.first_name,
+          last_name: $scope.contact.last_name,
+          phone_number: $scope.contact.phone_number,
+          email: $scope.contact.email,
+          reservations_attributes: [{
+            dining_table_id: $scope.dining_table_id ,
+            date: $scope.reservation_date,
+            observation: $scope.reservation_observation
+          }]
+        }
         $http({
             method: 'POST',
             url: '/contacts',
-            data: { contact: $scope.contact }
+            data: { contact: params }
             }).
         success(function (data) {
             //Showing Success message
@@ -170,7 +137,7 @@ bookIt.controller('FormController', ['$scope', '$http', function($scope, $http) 
             //Showing error message
             $scope.status = 'Unable to create a contact: ' + error.message;
         });
-        
+
       }
 
       $scope.reset = function() {
